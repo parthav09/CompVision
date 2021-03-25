e232b14f66105859f7a07aa24c301f8a4c1033ef
Parthav Joshi <parthavjoshi1718@gmail.com>
2021-03-25 23:37:54 +0530

    Update otsu_thresholding.py

--- module1/otsu_thresholding.py
+++ module1/otsu_thresholding.py
@@ -11,7 +11,6 @@ blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 
 # apply Otsu's automatic thresholding -- Otsu's method automatically
 # determines the best threshold value `T` for us
-#here 0 acts as a dont care parameter as in otsu the T is automatically computed
 (T, threshInv) = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
 print("Threshhold value is {}".format(T))
 cv2.imshow("THreshhold image ", cv2.bitwise_and(image, image, mask=threshInv))
