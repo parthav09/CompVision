4efd11eac6429a08b8cd3fdc4053f4da166d2c76
Parthav Joshi <parthavjoshi1718@gmail.com>
2021-04-15 21:34:06 +0530

    Color Histograms added

---
+++ module1/color_histogram.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+import argparse
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+# load the image and show it
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+# grab the image channels, initialize the tuple of colors and the
+# figure
+chans = cv2.split(image)
+colors = ("b", "g", "r")
+plt.figure()
+plt.title("'Flattened' Color Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+# loop over the image channels
+for (chan, color) in zip(chans, colors):
+	# create a histogram for the current channel and plot it
+	hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+	plt.plot(hist, color = color)
+	plt.xlim([0, 256])
+plt.show()
