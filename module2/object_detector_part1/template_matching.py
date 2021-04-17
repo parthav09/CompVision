ccac7c9d07037703f0c1a801389f5b554dada3c8
Parthav Joshi <parthavjoshi1718@gmail.com>
2021-04-17 22:17:05 +0530

    Update template_matching.py

--- module2/object_detector_part1/template_matching.py
+++ module2/object_detector_part1/template_matching.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 import argparse
 import cv2
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--source", required=True, help="Path to the source image")
@@ -13,3 +14,9 @@ template = cv2.imread(args["template"])
 # find the template in the source image
 result = cv2.matchTemplate(source, template, cv2.TM_CCOEFF)
 (minVal, maxVal, minLoc, (x, y)) = cv2.minMaxLoc(result)
+
+cv2.rectangle(source, (x, y), (x + tempW, y + tempH), (0, 255, 0), 2)
+
+cv2.imshow("Source: ", source)
+cv2.imshow("template: ", template)
+cv2.waitKey(0)
