fcd198443887d9f8c0aa41dc4c7f0d0408f7e6d1
Parthav Joshi <parthavjoshi1718@gmail.com>
2021-04-21 21:19:01 +0530

    updated

--- module2/Object_detection/train_detector.py
+++ module2/Object_detection/train_detector.py
@@ -17,3 +17,34 @@ ap.add_argument("-a", "--annotations", required=True,
 ap.add_argument("-o", "--output", required=True,
 	help="Path to the output detector")
 args = vars(ap.parse_args())
+print("[INFO] gathering images and bounding boxes...")
+options = dlib.simple_object_detector_training_options()
+images = []
+boxes = []
+# loop over the image paths
+for imagePath in paths.list_images(args["class"]):
+	# extract the image ID from the image path and load the annotations file
+	imageID = imagePath[imagePath.rfind("/") + 1:].split("_")[1]
+	imageID = imageID.replace(".jpg", "")
+	p = "{}/annotation_{}.mat".format(args["annotations"], imageID)
+	annotations = loadmat(p)["box_coord"]
+	# loop over the annotations and add each annotation to the list of bounding
+	# boxes
+	bb = [dlib.rectangle(left=long(x), top=long(y), right=long(w), bottom=long(h))
+			for (y, h, x, w) in annotations]
+	boxes.append(bb)
+	# add the image to the list of images
+	images.append(io.imread(imagePath))
+print("[INFO] training detector...")
+detector = dlib.train_simple_object_detector(images, boxes, options)
+# dump the classifier to file
+print("[INFO] dumping classifier to file...")
+detector.save(args["output"])
+# visualize the results of the detector
+win = dlib.image_window()
+win.set_image(detector)
+dlib.hit_enter_to_continue()
+
+
+# python3 train_detector.py --class stop_sign_images --annotations stop_sign_annotations --output output/stop_sign_setector.svm
+
